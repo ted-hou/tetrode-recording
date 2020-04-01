db3bf0625cdd981bcdd5e827384a795c6429d495
Lingfeng Hou <lingfenghou@g.harvard.edu>
2020-03-31 21:16:51 -0400

    Revert "try catch"
    This reverts commit e8da5486adf2e4ff4d0bd9453a5476b0979e1f2c.

--- TetrodeRecording.m
+++ TetrodeRecording.m
@@ -4653,22 +4653,18 @@ classdef TetrodeRecording < handle
 			readWindow = p.Results.Window * 0.001;
 
 			for iTr = 1:length(TR)
-                try
-                    startTime = tic();
-                    TetrodeRecording.TTS(['Processing file ', num2str(iTr), '/', num2str(length(TR)), '...']);
-                    StimData = TR(iTr).ReadStimData(PTR, [TR(iTr).Spikes.Channel], 'Window', readWindow);
-                    StimData = SerializeStimData(StimData);
+				startTime = tic();
+				TetrodeRecording.TTS(['Processing file ', num2str(iTr), '/', num2str(length(TR)), '...']);
+				StimData = TR(iTr).ReadStimData(PTR, [TR(iTr).Spikes.Channel], 'Window', readWindow);
+				StimData = SerializeStimData(StimData);
 
-                    filename = strsplit(TR(iTr).Files{1}, 'nev');
-                    filename = [TR(iTr).Path, '\..\SpikeSort\sd_', filename{1}, '.mat'];
+				filename = strsplit(TR(iTr).Files{1}, 'nev');
+				filename = [TR(iTr).Path, '\..\SpikeSort\sd_', filename{1}, '.mat'];
 
-                    save(filename, 'StimData', '-v7.3');
+				save(filename, 'StimData', '-v7.3');
 
-                    duration = toc(startTime);
-                    TetrodeRecording.TTS([num2str(duration), ' s.\n']);
-                catch
-                    warning(['Failed when attempting to process ', filename]);
-                end
+				duration = toc(startTime);
+				TetrodeRecording.TTS([num2str(duration), ' s.\n']);
 			end
 		end
 
