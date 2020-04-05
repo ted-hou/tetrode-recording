6b1fac08b505584b0e1efa47e488a4cbe83ddf4c
TedHo <tedhou1993@gmail.com>
2020-04-05 00:45:38 -0400

    Cool beans. CollisionTest.batch() seems to work

--- CollisionTest.m
+++ CollisionTest.m
@@ -9,6 +9,7 @@ classdef CollisionTest < handle
         TrainOff = []
         Window = [0, 0]
         Filename
+        ExpName = ''
     end
 
     properties (Transient, Access = {})
@@ -20,16 +21,18 @@ classdef CollisionTest < handle
     methods
         % Constructor
         function obj = CollisionTest(varargin)
-            %CollisionTest - Class for analyzing antidromic stimulation/and runnung collision tests.
-            %
-            % Syntax: ct = CollisionTest()
-            % 
-            % Optional Parameters: 
-            % 'Folder'  - Skips file selection prompt if a valid path is provided.
-            % 'Read'    - (Default TRUE) Whether or not to read tr/ptr/ns5 files. 
-            % 
-            %
-            % Class for analyzing antidromic stimulation/and runnung collision tests.
+        %CollisionTest - Class for analyzing antidromic stimulation/and runnung collision tests.
+        %
+        % Syntax: ct = CollisionTest()
+        % 
+        % Optional Parameters: 
+        % 'Folder'          - Skips file selection prompt if a valid path is provided.
+        % 'Read'            - (Default TRUE) Whether or not to read tr/ptr/ns5 files. 
+        % 'Channels'        - Which channels to look at. This is channel ID as shown in TetrodeRecording.PlotChannel([]). Leave empty to use all channels.
+        % 'MaxTrains'       - Limit how many stim trains will be read.
+        % 'ExtendedWindow'  - (ms) Extended window for reading data.
+        % 
+        % 
             p = inputParser();
             p.addParameter('Folder', '', @ischar);
             p.addParameter('Read', true, @islogical);
@@ -47,44 +50,10 @@ classdef CollisionTest < handle
                 folder = folder{1};
             end
 
-            % Check folder for NSx files
-            if (isfolder(folder))
-                nsx = dir(sprintf('%s\\*.ns5', folder));
-                if length(nsx) > 1
-                    nsx = nsx(1);
-                    warning('More than one .ns5 file is detected in "%s". Only the first one is used. This may lead to unexpected results.', folder)
-                elseif length(nsx) < 1
-                    error('No .ns5 file detected in "%s".', folder)
-                end
-            else
-                error('Selected path "%s" is not a folder.', folder)
-            end
-
-            % Determine experiment name
-            expName = strsplit(nsx.name, '.ns5');
-            expName = expName{1};
-
-            % Find corresponding TR/PTR files in SpikeSort folder.
-            spikeSortFolder = sprintf('%s\\..\\SpikeSort', nsx.folder);
-            tr = dir(sprintf('%s\\tr_%s*.mat', spikeSortFolder, expName));
-            ptr = dir(sprintf('%s\\ptr_%s.mat', spikeSortFolder, expName));
-
-            % Make sure TR/PTR files exist
-            if isempty(tr) || isempty(ptr)
-                error('Some of the following files could not be found: \n\t"tr_%s.mat"; \n\t"ptr_%s.mat".', expName)
-            end
-
-            if (length(ptr) > 1) 
-                ptr = ptr(1);
-                warning('More than one (%d) ptr file was found.', length(ptr));
-            end
-
-            % Store file names
-            obj.Filename.NSx = sprintf('%s\\%s', nsx.folder, nsx.name);
-            obj.Filename.NEV = sprintf('%s\\%s.nev', nsx.folder, expName);
-            obj.Filename.PTR = sprintf('%s\\%s', ptr.folder, ptr.name);
-            for iTr = 1:length(tr)
-                obj.Filename.TR{iTr} = sprintf('%s\\%s', tr(iTr).folder, tr(iTr).name);
+            % Validate folder
+            [~, obj.Filename, obj.ExpName, allValid] = CollisionTest.validateFolders(folder, 'SuppressWarnings', true);
+            if ~allValid
+                error('Invalid folder %s', folder);
             end
 
             if (doRead)
@@ -155,7 +124,7 @@ classdef CollisionTest < handle
                 if (iPulseInPage >= tracesPerPage)
                     stimPatchVertices = vertcat(stimOnVertices, stimOffVertices(end:-1:1, :));
                     patch('XData', stimPatchVertices(:, 1), 'YData', stimPatchVertices(:, 2), 'FaceColor', '#4DBEEE', 'FaceAlpha', 0.33, 'EdgeAlpha', 0);
-                    ylim([(iPulse - iPulseInPage + 1) * ySpacing, iPulse * ySpacing + 1])
+                    ylim([(iPulse - iPulseInPage + 1) * ySp, iPulse * ySpacing + 1])
                     title(ax, sprintf('Pulses %d - %d', iPulse - iPulseInPage + 1, iPulse))
                     [~, ~, button] = ginput(1);
                     
@@ -172,9 +141,68 @@ classdef CollisionTest < handle
 
             hold(ax, 'off');
         end
+
+        function save(obj, varargin)
+        %save - Save the object to a .mat file.
+        %
+        % Syntax: save(obj, varargin)
+        % 
+            p = inputParser();
+            p.addOptional('Filename', '', @ischar);
+            p.parse(varargin{:});
+            filename = p.Results.Filename;
+
+            if (isempty(filename))
+                uisave('obj', sprintf('ct_%s.mat', obj.ExpName));
+            else
+                fprintf('Saving to file "%s"...', filename)
+                save(filename, 'obj', '-v7.3');
+                fprintf('Done.\n')
+            end
+        end
     end
 
-    % Private methods
+    % Public static methods
+    methods (Static)
+        function batch(varargin)
+        %Batch - Batch generate and save CollisionTest objects from tr/ptr/ns5 files.
+        %
+        % Optional Parameters: 
+        % 'ExtendedWindow'  - (ms) Extended window for reading data.
+        % 
+        % Syntax: Batch(varargin)
+        %
+            p = inputParser();
+            p.addParameter('ExtendedWindow', [-20, 20], @(x) isnumeric(x) && length(x) == 2 && diff(x) > 0);
+            p.parse(varargin{:});
+
+            folders = uipickfiles('Prompt', 'Select multiple folders each containing an ns5 file.');
+
+            folders = CollisionTest.validateFolders(folders, 'SuppressWarnings', false);
+
+            for iFolder = 1:length(folders)
+                folder = folders{iFolder};
+                fprintf('Processing folder %d of %d - %s:\n', iFolder, length(folders), folder);
+
+                ct = CollisionTest('Folder', folder, 'ExtendedWindow', p.Results.ExtendedWindow);
+
+                % Make sure save path exists
+                saveFolder = sprintf('%s//..//CollisionTest', folder);
+                if ~isfolder(saveFolder)
+                    mkdir(saveFolder);
+                end
+                ct.save(sprintf('%s//ct_%s.mat', saveFolder, ct.ExpName));
+            end
+        end
+
+        function load()
+        %Load - Load multiple ct files.
+        %
+            files = uipickfiles('Prompt', 'Select (multiple) .mat files containing an CollisionTest object named "obj"', 'FilterSpec', '*.mat');
+        end
+    end
+
+    % Private methodd
     methods (Access = {})
         function read(obj, channels, maxTrains, extendedWindow)
         %read - Read data from tr/ptr/ns5 files.
@@ -207,6 +235,10 @@ classdef CollisionTest < handle
                 trainOff = trainOff(1:maxTrains);
             end
 
+            if isempty(channels)
+                channels = [obj.TR.Spikes.Channel];
+            end
+
             sampleRate = obj.TR.FrequencyParameters.AmplifierSampleRate;
 
             % Pre-allocate
@@ -214,8 +246,8 @@ classdef CollisionTest < handle
             maxTrainLength = max(trainOff - trainOn) + diff(extendedWindow);
             maxTrainLength = ceil(sampleRate * maxTrainLength);
 
-            dataByTrain = zeros(length(trainOn), maxTrainLength, length(channels));
-            timestampsByTrain = zeros(length(trainOn), maxTrainLength);
+            dataByTrain = NaN(length(trainOn), maxTrainLength, length(channels));
+            timestampsByTrain = NaN(length(trainOn), maxTrainLength);
 
             % Read data from each train and put them in an array.
             sysInitDelay = obj.TR.FrequencyParameters.SysInitDelay.Duration; % Get sysInitDelay to shift openNSx window to the correct value.
@@ -231,18 +263,19 @@ classdef CollisionTest < handle
 
                 NSx = openNSx(obj.Filename.NSx, 'read', 'channels', nsxChannels, 'duration', readWindow, 'sec');
                 numSamples = size(NSx.Data, 2);
-                firstSampleIndex = floor(NSx.MetaTags.Timestamp);
 
                 dataByTrain(iTrain, 1:numSamples, :) = reshape(transpose(NSx.Data), 1, numSamples, []);
-                timestampsByTrain(iTrain, 1:numSamples) = (firstSampleIndex : firstSampleIndex + numSamples - 1) / sampleRate;
+                % timestampsByTrain(iTrain, 1:numSamples) = (firstSampleIndex : firstSampleIndex + numSamples - 1) / sampleRate;
+                % timestampsByTrain(iTrain, 1:numSamples) = readWindow(1):1/sampleRate:readWindow(2);
+                timestampsByTrain(iTrain, 1:numSamples) = readWindow(1):1/sampleRate:readWindow(1) + (numSamples - 1) * 1/sampleRate;
             end
 
             data = reshape(permute(dataByTrain, [2, 1, 3]), size(dataByTrain, 1) * size(dataByTrain, 2), []);
             timestamps = reshape(permute(timestampsByTrain, [2, 1]), [], 1);
 
-            notZero = timestamps > 0;
-            data = data(notZero, :);
-            timestamps = timestamps(notZero);
+            hasData = ~isnan(timestamps);
+            data = data(hasData, :);
+            timestamps = timestamps(hasData);
 
             if (~CollisionTest.validateTimestamps(timestamps))
                 error('Timestamps are not monotonic increasing. Digital events data probably needs to be trimmed.')
@@ -264,7 +297,7 @@ classdef CollisionTest < handle
         function nsxChannels = mapChannels(obj, channels)
         %mapChannels - Convert TR channel labels (1:n continuous) to NSx file channels (1:N continuous). n: number of spike sorted channels. N: number of recorded channels including both rigs.
 
-        % Find out which rig we're using. Rig2 data is appended after rig1.
+            % Find out which rig we're using. Rig2 data is appended after rig1.
             rig = TetrodeRecording.GetRig(obj.Filename.NSx);
             if (rig == 1)
                 channelMap = obj.PTR.ChannelMap.Rig1;
@@ -278,13 +311,92 @@ classdef CollisionTest < handle
         end
     end
 
-    % Static methods
+    % Static private methods
     methods (Access = {}, Static)
-        
+
         function isMonoIncrease = validateTimestamps(array)
             isIncrease = diff(array) > 0;
             isMonoIncrease = nnz(isIncrease) == length(array) - 1;
         end
-        
+
+        function varargout = validateFolders(folders, varargin)
+            p = inputParser();
+            p.addRequired('Folders', @(x) iscell(x) || ischar(x));
+            p.addParameter('SuppressWarnings', false, @islogical);
+            p.parse(folders, varargin{:});
+            folders = p.Results.Folders;
+            suppressWarnings = p.Results.SuppressWarnings;
+
+            allValid = true;
+            validFolders = {};
+
+            % Convert single folder in string format to cell.
+            if ischar(folders)
+                folders = {folders};
+            end
+
+            % Make sure all folders are valid, filter out bad ones.
+            for iFolder = 1:length(folders)
+                folder = folders{iFolder};
+
+                % Check folder for NSx files
+                if (isfolder(folder))
+                    nsx = dir(sprintf('%s\\*.ns5', folder));
+                    if length(nsx) > 1
+                        nsx = nsx(1);
+                        if ~suppressWarnings
+                            warning('More than one .ns5 file is detected in "%s" but only the first one is used. This may lead to unexpected results.', folder)
+                        end
+                    elseif length(nsx) < 1
+                        if ~suppressWarnings
+                            warning('No .ns5 file detected in "%s".', folder)
+                        end
+                        allValid = false;
+                        continue
+                    end
+                else
+                    if ~suppressWarnings
+                        warning('Selected path "%s" is not a folder.', folder)
+                    end
+                end
+
+                % Determine experiment name
+                expName = strsplit(nsx.name, '.ns5');
+                expName = expName{1};
+
+                % Find corresponding TR/PTR files in SpikeSort folder.
+                spikeSortFolder = sprintf('%s\\..\\SpikeSort', nsx.folder);
+                tr = dir(sprintf('%s\\tr_%s*.mat', spikeSortFolder, expName));
+                ptr = dir(sprintf('%s\\ptr_%s.mat', spikeSortFolder, expName));
+
+                % Make sure TR/PTR files exist
+                if isempty(tr) || isempty(ptr)
+                    if ~suppressWarnings
+                        warning('Some of the following files could not be found: \n\t"tr_%s.mat"; \n\t"ptr_%s.mat".', expName)
+                    end
+                    allValid = false;
+                    continue
+                end
+
+                if (length(ptr) > 1) 
+                    ptr = ptr(1);
+                    if ~suppressWarnings
+                        warning('More than one (%d) ptr file was found but only the first one is used. This may lead to unexpected results.', length(ptr));
+                    end
+                end
+
+                validFolders = [validFolders, folder];
+
+                % Store file names
+                Filename(iFolder).NSx = sprintf('%s\\%s', nsx.folder, nsx.name);
+                Filename(iFolder).NEV = sprintf('%s\\%s.nev', nsx.folder, expName);
+                Filename(iFolder).PTR = sprintf('%s\\%s', ptr.folder, ptr.name);
+                for iTr = 1:length(tr)
+                    Filename(iFolder).TR{iTr} = sprintf('%s\\%s', tr(iTr).folder, tr(iTr).name);
+                end
+            end
+
+            varargout = {validFolders, Filename, expName, allValid};
+        end
     end
 end
