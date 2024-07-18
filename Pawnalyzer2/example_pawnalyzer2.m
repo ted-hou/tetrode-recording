ea0752c853f8598af4137580890df16c71272804
Lingfeng Hou <lingfenghou@g.harvard.edu>
2024-07-18 18:12:35 -0400

    Update example_pawnalyzer2.m

--- Pawnalyzer2/example_pawnalyzer2.m
+++ Pawnalyzer2/example_pawnalyzer2.m
@@ -26,14 +26,24 @@ pa.getClips(nFramesBefore=15, nFramesAfter=0, keepData=false, trials='PressSpont
 % restart. Your labelled data should not be affected unless you exit MATLAB
 pa.start();
 
-% 2.3 Press Ctrl+S or type pa.save(); in the command window to save your 
+% Some keybinds to remember
+%   Left click for Contra (red circle), right click for Ipsi (yellow circle)
+%   Click and drag to move the circle
+%   Alt + Click and drag to precisely move the circle
+%   Capslock + Click to turn on tracking mode
+%   Mousewheel or arrow keys to change frame.
+%   Shift + Mousewheel or Shift + arrow key to change trial.
+%   Z or C to go to the first or last frame in the trial.
+%   Ctrl+S to save, see step 2.3 below
+
+% 2.3 Press "Ctrl+S" or type "pa.save()" in the command window to save your 
 % progress. Unsaved progress will be lost when MATLAB is closed so SAVE OFTEN!
 % The first time you save (for each session) the program will ask where you
 % want to save the file, the default path should be fine, just make sure
 % each session is saved to its own file so you don't overwrite your progress.
 
 % 2.4 If MATLAB is restarted, rerun the script from the top until you get
-% here, then type pa.load() in the command window, select your previously saved data. 
+% here, then type "pa.load()" in the command window, select your previously saved data. 
 
 %% 3. You have finished labelling one sessions, now return to Step 2.1
 % Make sure you change the value of 'iExp' on line 20. This determines
