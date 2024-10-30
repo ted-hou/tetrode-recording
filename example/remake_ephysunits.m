0a260054ef45ae49e7aa9bb6ba9d1fdb1e723856
ted-hou <tedhou1993@gmail.com>
2024-10-30 17:09:44 -0400

    Update remake_ephysunits.m

--- example/remake_ephysunits.m
+++ example/remake_ephysunits.m
@@ -41,3 +41,25 @@ end
 % euMixed(lia) = euOld(locb(lia));
 % 
 % %%
+
+%% Load euComplete (complete with ITI spikes)
+
+files = dir('C:\SERVER\Units\NonLite_PressVsLick\*.mat');
+sel = ismember(cellfun(@(n) lower(strrep(n, '.mat', '')), {files.name}, UniformOutput=false), euNames);
+files = files(sel);
+cd('C:\SERVER\Units\NonLite_PressVsLick\')
+euComplete = EphysUnit.load({files.name});
+
+% euComplete.save('C:\SERVER\Units\NonLite_PressVsLick_NonDuplicate_NonDrift');
+
+%% Make ETA
+euMixed = eu;
+
+[lia, locb] = ismember(euComplete.getName(), eu.getName());
+euMixed(lia) = euComplete(locb(lia));
+
+%%
+eta.lickBoutEnd = euMixed.getETA('count', 'lickboutend', window=[0, 2], resolution=[2*pi/12, 0.01], normalize='none', ...
+    maxInterval=0.2, minInterval=0.05, minBoutCycles=8);
+eta.lickBoutEndNorm = euMixed.getETA('count', 'lickboutend', window=[0, 2], resolution=[2*pi/12, 0.01], normalize=[1, 2], ...
+    maxInterval=0.2, minInterval=0.05, minBoutCycles=8);
