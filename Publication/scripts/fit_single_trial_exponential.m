626c7b1899a5878e31302d12feb54c7c1ace26a4
ted-hou <ted_hou@126.com>
2025-03-04 15:22:03 -0500

    implemented loglinear fit (fast)
    but fit allows larger errors when y is large (i.e., during peak of ramping), therefore is much worse at fitting ramps.
    
    Would have to try a faster method of exponential fitting, or by ommiting bootstraps

--- Publication/scripts/fit_single_trial_exponential.m
+++ Publication/scripts/fit_single_trial_exponential.m
@@ -4,31 +4,82 @@ pSTE.kernel = struct(type='exponential', params=struct(lambda1=5, lambda2=10, re
 pSTE.minTrialLength = 1.5;
 pSTE.minNumTrials = 20;
 pSTE.cueBlankWindow = 0.5;
-pSTE.fitWindow = [-2, 0];
+pSTE.fitWindow = [-0.5, 0];
 pSTE.baselineWindow = [-4, -2];
 pSTE.nBoot = 10000;
-pSTE.nTrialsPerBin = 5;
+pSTE.nTrialsPerBin = 4;
 
-iEu = find(c.hasPress & c.hasLick & c.isLickUp, 1, 'first');
+close all
+fig = figure(Units='inches', Position=[3 3 12 6]);
+tl = tiledlayout(fig, 2, 3);
+ax = gobjects(2, 3);
+for i = 1:2
+    for j = 1:3
+        ax(i, j) = nexttile(tl);
+    end
+end
 
-% Get PETH
-trials = eu(iEu).getTrials('lick');
-trials = trials(trials.duration() >= pSTE.minTrialLength);
-assert(length(trials) >= pSTE.minNumTrials)
-[X, t] = eu(iEu).getTrialAlignedData('rate', window=[-4, 2], trialType=trialType, resolution=0.01, trials=trials, allowedTrialDuration=[0, Inf], includeInvalid=false, startBlankWindow=[-Inf, pSTE.cueBlankWindow], kernel=pSTE.kernel);
-z = trials.duration();
+clear obs boot ci95 ci99 pValue
 
-% Normalize PETH
-inBaselineWindow = t >= -4 & t <= -2;
-inFitWindow = t >= -2 & t <= 0;
-mu = mean(X(:, inBaselineWindow), 'all', 'omitnan');
-sigma = std(X(:, inBaselineWindow), 0, 'all', 'omitnan');
-X = (X - mu) ./ sigma;
+trialType = 'lick';
+euIndices = find(c.hasPress & c.hasLick & c.isLickDown);
+isUp = false;
+for i = 1:length(euIndices)
+    for iAx = 1:6
+        cla(ax(iAx))
+    end
 
-tic;
-[obs, boot, ci95, ci99] = bootSingleTrialExponential(X(:, inFitWindow), t(:, inFitWindow), z, nBoot=pSTE.nBoot, nTrialsPerBin=pSTE.nTrialsPerBin, isUp=true);
-toc;
-%%
+    iEu = euIndices(i);
+    
+    % Get PETH
+    trials = eu(iEu).getTrials(trialType);
+    trials = trials(trials.duration() >= pSTE.minTrialLength);
+    assert(length(trials) >= pSTE.minNumTrials)
+    [X, t] = eu(iEu).getTrialAlignedData('rate', window=[-4, 2], trialType=trialType, resolution=0.01, trials=trials, allowedTrialDuration=[0, Inf], includeInvalid=false, startBlankWindow=[-Inf, pSTE.cueBlankWindow], kernel=pSTE.kernel);
+    z = trials.duration();
+    
+    % Normalize PETH
+    inBaselineWindow = t >= pSTE.baselineWindow(1) & t <= pSTE.baselineWindow(2);
+    inFitWindow = t >= pSTE.fitWindow(1) & t <= pSTE.fitWindow(2);
+    mu = mean(X(:, inBaselineWindow), 'all', 'omitnan');
+    sigma = std(X(:, inBaselineWindow), 0, 'all', 'omitnan');
+    X = (X - mu) ./ sigma;
+
+    % Have to deal with negative numbers using loglinear fit
+    % Here we just take the absolute (which removes the need to reverse the
+    % sign for tau for decrease units)
+    if ~isUp
+        X = -X;
+    end
+    X(X<=0) = 0.1;
+
+%     X = (X - min(X, [], 2, 'omitnan')) ./ (max(X, [], 2, 'omitnan') - min(X, [], 2, 'omitnan'));
+    
+    tic;
+    [obs, boot, ci95, ci99, pValue] = bootSingleTrialExponential(X(:, inFitWindow), t(:, inFitWindow), z, nBoot=pSTE.nBoot, nTrialsPerBin=pSTE.nTrialsPerBin);
+    toc;
+
+    nBins = length(obs.z);
+    scatter(ax(1, 1), obs.z, obs.tau, 25, getColor(1:nBins, nBins, 0.8))
+    title(ax(1, 1), sprintf('tau\nobs=%g, ci95=[%g, %g]', obs.slope.tau, ci95.tau(1), ci95.tau(2)))
+
+    scatter(ax(2, 1), obs.z, obs.a, 25, getColor(1:nBins, nBins, 0.8))
+    title(ax(2, 1), sprintf('a\nobs=%g, ci95=[%g, %g]', obs.slope.a, ci95.a(1), ci95.a(2)))
+
+    hold(ax(:, 2:3), 'on')
+    for iBin = 1:nBins
+        plot(ax(1, 2), t(inFitWindow)', log(obs.X(iBin, :)'), Color=[getColor(iBin, nBins, 0.8), 0.5], LineWidth=1.5)
+        plot(ax(2, 2), t(inFitWindow)', log(obs.a(iBin)') + t(inFitWindow)'./obs.tau(iBin)', Color=[getColor(iBin, nBins, 0.8), 0.5], LineWidth=1.5)
+        plot(ax(1, 3), t(inFitWindow)', obs.X(iBin, :)', Color=[getColor(iBin, nBins, 0.8), 0.5], LineWidth=1.5)
+        plot(ax(2, 3), t(inFitWindow)', obs.a(iBin)'.*exp(t(inFitWindow)'./obs.tau(iBin)'), Color=[getColor(iBin, nBins, 0.8), 0.5], LineWidth=1.5)
+    end
+    hold(ax(:, 2:3), 'off')
+%     ylim(ax(:, 3), [0, 5])
+
+    title(ax(1, 2), 'Log Scale')
+    title(ax(1, 3), 'Real Scale')
+end
+%
 % X = spike rate (nTrials, nTimestampsPerTrial), t = timestamp (1, nTimestampsPerTrial), z = movement time (nTrials, 1)
 function [obs, boot, ci95, ci99, pValue] = bootSingleTrialExponential(X, t, z, varargin)
     p = inputParser();
@@ -37,7 +88,6 @@ function [obs, boot, ci95, ci99, pValue] = bootSingleTrialExponential(X, t, z, v
     p.addRequired('z', @isnumeric)
     p.addParameter('nBoot', 100, @(n) n>=3);
     p.addParameter('nTrialsPerBin', 5, @isnumeric);
-    p.addParameter('isUp', true, @islogical);
     p.parse(X, t, z, varargin{:})
     r = p.Results;
     k = r.nTrialsPerBin;
@@ -49,17 +99,6 @@ function [obs, boot, ci95, ci99, pValue] = bootSingleTrialExponential(X, t, z, v
     assert(length(z) == nTrials);
     t = reshape(t, 1, []);
     z = reshape(z, [], 1);
-
-    if r.isUp
-        modelSpec = fittype(@(a, b, c, t) a*exp(b*t)+c, dependent='x', independent='t');
-    else
-        modelSpec = fittype(@(a, b, c, t) -a*exp(b*t)+c, dependent='x', independent='t');
-    end
-
-%     bootSlope.args = struct(nBoot=nBoot, nTrialsPerBin=r.nTrialsPerBin);
-%     bootSlope.tau = struct(ci95=[], ci99=[], p=[]);
-%     bootSlope.a = struct(ci95=[], ci99=[], p=[]);
-%     bootSlope.c = struct(ci95=[], ci99=[], p=[]);
 
     % Order trials by movement time
     [~, trialOrderSorted] = sort(z, 'ascend');
@@ -78,30 +117,31 @@ function [obs, boot, ci95, ci99, pValue] = bootSingleTrialExponential(X, t, z, v
     % Fit exponential to each binned PETH
     tau=NaN(nBins, 1);
     a=NaN(nBins, 1);
-    c=NaN(nBins, 1);
     for iBin = 1:nBins
         isValid = ~isnan(XBinned(iBin, :));
-        mdl = fit(t(isValid)', XBinned(iBin, isValid)', modelSpec, StartPoint=[1, 10, 0], Lower=[0.1, 0.1, -5], Upper=[10, 100, 5]); % Starting points and bounds for a and c assumes normalized spike rates
-        tau(iBin) = 1./mdl.b;
-        a(iBin) = mdl.a;
-        c(iBin) = mdl.c;
+        tt = t(isValid)';
+        xx = XBinned(iBin, isValid)';
+        coeff = fastfitlm([ones(size(tt)), tt], log(xx));
+        a(iBin) = exp(coeff(1));
+        tau(iBin) = 1/coeff(2);
     end
 
-    obs = struct(X=XBinned, z=zBinned, tau=tau, a=a, c=c, slope=struct(tau=[], a=[], c=[]));
+    obs = struct(X=XBinned, z=zBinned, tau=tau, a=a, slope=struct(tau=[], a=[]));
 
     % Estimate the slope of tau/a/c vs. z
-    paramNames = ["tau", "a", "c"];
-    for iParam = 1:3
-        paramName = paramNames(iParam);
-        mdl = fitlm(obs.z, obs.(paramName), VarNames=["z", paramName]);
-        obs.slope.(paramName) = table2array(mdl.Coefficients('z', 'Estimate'));
-    end
+    phi = [ones(size(obs.z)), obs.z]; % design matrix with column of ones and movement times, i.e. phi
+    assert(nnz(isnan(obs.z)) == 0)
+    assert(nnz(isnan(obs.tau)) == 0)
+    assert(nnz(isnan(obs.a)) == 0)
+    coeff = fastfitlm(phi, obs.tau);
+    obs.slope.tau = coeff(2);
+    coeff = fastfitlm(phi, obs.a);
+    obs.slope.a = coeff(2);
 
     % Bootstrap(permute) by shuffling z and re-estimating slopes
     tau=NaN(nBins, nBoot);
     a=NaN(nBins, nBoot);
-    c=NaN(nBins, nBoot);
-    boot = struct(tau=NaN(1, nBoot), a=NaN(1, nBoot), c=NaN(1, nBoot));
+    boot = struct(tau=NaN(1, nBoot), a=NaN(1, nBoot));
     for iBoot = 1:nBoot
         zShuffled = z(randperm(nTrials));
         [~, trialOrderShuffled] = sort(zShuffled, 'ascend');
@@ -115,28 +155,41 @@ function [obs, boot, ci95, ci99, pValue] = bootSingleTrialExponential(X, t, z, v
         
         for iBin = 1:nBins
             isValid = ~isnan(XBinned(iBin, :));
-            mdl = fit(t(isValid)', XBinned(iBin, isValid)', modelSpec, StartPoint=[1, 10, 0], Lower=[0.1, 0.1, -5], Upper=[10, 100, 5]); % Starting points and bounds for a and c assumes normalized spike rates
-            tau(iBin, iBoot) = 1./mdl.b;
-            a(iBin, iBoot) = mdl.a;
-            c(iBin, iBoot) = mdl.c;
+            tt = t(isValid)';
+            xx = XBinned(iBin, isValid)';
+            coeff = fastfitlm([ones(size(tt)), tt], log(xx));
+            a(iBin, iBoot) = exp(coeff(1));
+            tau(iBin, iBoot) = 1/coeff(2);
         end
 
-        mdl = fitlm(zBinned, tau(:, iBoot), VarNames=["z", "tau"]);
-        boot.tau(iBoot) = table2array(mdl.Coefficients('z', 'Estimate'));
-        mdl = fitlm(zBinned, a(:, iBoot), VarNames=["z", "a"]);
-        boot.a(iBoot) = table2array(mdl.Coefficients('z', 'Estimate'));
-        mdl = fitlm(zBinned, c(:, iBoot), VarNames=["z", "c"]);
-        boot.c(iBoot) = table2array(mdl.Coefficients('z', 'Estimate'));
+        phi = [ones(size(zBinned)), zBinned]; % design matrix with column of ones and movement times, i.e. phi
+
+        sel = ~isinf(tau(:, iBoot)) & ~isnan(tau(:, iBoot)) & ~isinf(a(:, iBoot)) & ~isnan(a(:, iBoot));
+        coeff = fastfitlm(phi, tau(sel, iBoot));
+        boot.tau(iBoot) = coeff(2);
+
+        coeff = fastfitlm(phi, a(sel, iBoot));
+        boot.a(iBoot) = coeff(2);
     end
 
-    for iParam = 1:3
-        paramName = paramNames(iParam);
-        ci95.(paramName) = quantile(boot.(paramName), [0.025, 0.975]);
-        ci99.(paramName) = quantile(boot.(paramName), [0.005, 0.995]);
-        if obs.slope.(paramName) > median(boot.(paramName))
-            pValue.(paramName) = mean(boot.(paramName) > obs.slope.(paramName));
-        else
-            pValue.(paramName) = mean(boot.(paramName) < obs.slope.(paramName));
-        end
+    ci95.tau = quantile(boot.tau, [0.025, 0.975]);
+    ci99.tau = quantile(boot.tau, [0.005, 0.995]);
+    if obs.slope.tau > median(boot.tau)
+        pValue.tau = mean(boot.tau > obs.slope.tau) * 2;
+    else
+        pValue.tau = mean(boot.tau < obs.slope.tau) * 2;
     end
+
+    ci95.a = quantile(boot.a, [0.025, 0.975]);
+    ci99.a = quantile(boot.a, [0.005, 0.995]);
+    if obs.slope.a > median(boot.a)
+        pValue.a = mean(boot.a > obs.slope.a) * 2;
+    else
+        pValue.a = mean(boot.a < obs.slope.a) * 2;
+    end
+end
+
+%%
+function coeff = fastfitlm(X, y)
+    coeff = (X'*X)\X'*y;
 end
