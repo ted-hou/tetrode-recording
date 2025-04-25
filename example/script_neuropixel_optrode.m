f969bf778c14466e18fa2c96644f330d649b0085
ted-hou <ted_hou@126.com>
2025-04-25 15:53:07 -0400

    Update script_neuropixel_optrode.m

--- example/script_neuropixel_optrode.m
+++ example/script_neuropixel_optrode.m
@@ -3,15 +3,16 @@ tr = TetrodeRecording;
 tr.SelectFiles();
 tr.ReadFiles(Duration=120, NumSigmas=4, NumSigmasReturn=1.5, NumSigmasReject=20, WaveformWindow=[-0.5, 1])
 tr.SaveNeuropixelIO()
-%% Read detected spikes and NIDQ digital/analog channels
-tr.LoadNeuropixelIO();
+
+% Read detected spikes and NIDQ digital/analog channels
+% tr.LoadNeuropixelIO();
 tr.ParseNeuropixelIO();
-tr.LoadSpikes(1:384);
 
 % Spike sort
+tr.LoadSpikes(1:384);
 tr.SpikeSort(1:384, Dimension=10, FeatureMethod='PCA', WaveformWindow=[-0.5, 1], ClusterMethod='kmeans', NumClusters=3);
 tr.SaveSpikes(Path='Spikes_AutoSorted');
-
+%%
 tr.LoadSpikes(1:384, Path='Spikes_AutoSorted');
 
 %% Load sorted data on a different PC
