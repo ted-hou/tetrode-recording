e6da1f86688db25ffb6e593fc6739d59debf023b
ted-hou <ted_hou@126.com>
2025-05-23 16:27:50 -0400

    heh

--- example/script_neuropixel_optrode_SC.m
+++ example/script_neuropixel_optrode_SC.m
@@ -1,72 +1,39 @@
-%% Spike detection
-tr = TetrodeRecording;
-tr.SelectFiles();
-tr.ReadFiles(Duration=240, NumSigmas=4, NumSigmasReturn=1.5, NumSigmasReject=20, WaveformWindow=[-0.5, 1])
-tr.SaveNeuropixelIO()
-
-% Read detected spikes and NIDQ digital/analog channels
-% tr.LoadNeuropixelIO();
-tr.ParseNeuropixelIO();
-
-% Spike sort
-tr.LoadSpikes(1:384);
-tr.IterativeArtifactRemoval(1:384, MinSpikeRate=4, KIterative=4, KFinal=2, MaxIters=5, ...
-    DimensionIterative=3, DimensionFinal=10, FeatureMethod='PCA', ClusterMethod='kmeans', ...
-    WaveformWindow=[-0.5, 0.5]);
-tr.SaveSpikes(Path='Spikes_AutoSortedIterative');
 
 %% Spike detection
-tr = TetrodeRecording;
-tr.SelectFiles();
-tr.LoadNeuropixelIO();
-tr.ParseNeuropixelIO();
+folders = { ...
+    'C:\SERVER\daisy26\daisy26_20250521', ...
+    'C:\SERVER\desmond38\desmond38_20250520', ...
+    'C:\SERVER\desmond39\desmond38_20250522', ...
+    };
 
-% Spike sort
-tr.LoadSpikes(1:384, Path='Spikes');
-tr.SpikeSort(1:384, Dimension=10, FeatureMethod='PCA', WaveformWindow=[-0.5, 0.5], ClusterMethod='kmeans', NumClusters=3);
-tr.SpikeCullLowSpikeRateClusters(1:384, MinSpikeRate=1);
-tr.SpikeSort(1:384, Dimension=10, FeatureMethod='PCA', WaveformWindow=[-0.5, 0.5], ClusterMethod='kmeans', NumClusters=3);
-tr.SaveSpikes(Path='Spikes_AutoSorted');
-
-
-%% Load sorted data on a different PC
-clear, clc
-tr = TetrodeRecording();
-tr.SelectFiles();
-tr.LoadNeuropixelIO();
-tr.ParseNeuropixelIO();
-
-%
-% channels = 1:128;
-% tr.LoadSpikes(channels, Path='Spikes_AutoSortedIterative');
-% tr.PlotAllChannels(Channels=channels, plotMethod='mean')
-% %%
-% tr.SaveSpikes(Channels=channels, Path='Spikes_Sorted')
-channels = 129:256;
-tr.Spikes = [];
-tr.LoadSpikes(channels, Path='Spikes_AutoSortedIterative');
-tr.PlotAllChannels(Channels=channels, plotMethod='mean')
-%%
-tr.SaveSpikes(Channels=channels, Path='Spikes_Sorted')
-channels = 257:384;
-tr.Spikes = [];
-tr.LoadSpikes(channels, Path='Spikes_AutoSortedIterative');
-tr.PlotAllChannels(Channels=channels, plotMethod='mean')
-%%
-tr.SaveSpikes(Channels=channels, Path='Spikes_Sorted')
-
+for iSession = 1:length(folders)
+    try
+        tr = TetrodeRecording;
+        tr.SelectFiles(NeuropixelPath=folders{iSession});
+        tr.ReadFiles(Duration=240, NumSigmas=4, NumSigmasReturn=1.5, NumSigmasReject=20, WaveformWindow=[-0.5, 1])
+        tr.SaveNeuropixelIO()
+        
+        % Read detected spikes and NIDQ digital/analog channels
+        % tr.LoadNeuropixelIO();
+        tr.ParseNeuropixelIO();
+        
+        % Spike sort
+        tr.LoadSpikes(1:384);
+        tr.IterativeArtifactRemoval(1:384, MinSpikeRate=0.5, KIterative=4, KFinal=2, MaxIters=5, ...
+            DimensionIterative=3, DimensionFinal=10, FeatureMethod='PCA', ClusterMethod='kmeans', ...
+            WaveformWindow=[-0.5, 0.5]);
+        tr.SaveSpikes(Path='Spikes_AutoSortedIterative');
+    catch ME
+        warning('Could not process folder: %s', folders{iSession})
+        warning('Error in program %s.\nTraceback (most recent at top):\n%s\nError Message:\n%s', mfilename, getcallstack(ME), ME.message)
+    end
+end
 
 %% IterativeArtifactRemoval (OnionPeeling)
 folders = { ...
-    % 'C:\SERVER\desmond38\desmond38_20250401', ...
-    % 'C:\SERVER\desmond38\desmond38_20250402', ...
-    % 'C:\SERVER\desmond38\desmond38_20250403', ...
-%     'C:\SERVER\desmond38\desmond38_20250407', ...
-    % 'C:\SERVER\desmond38\desmond38_20250417', ...
-    'C:\SERVER\desmond39\desmond39_20250404', ...
-%     'C:\SERVER\desmond39\desmond39_20250407', ...
-%     'C:\SERVER\desmond39\desmond39_20250408', ...
-%     'C:\SERVER\desmond39\desmond39_20250423', ...
+    'C:\SERVER\daisy26\daisy26_20250519', ...
+    % 'C:\SERVER\desmond38\desmond38_20250520', ...
+    % 'C:\SERVER\desmond39\desmond38_20250522', ...
     };
 
 chunkSize = 48; % NumChannelsPerChunk
@@ -80,7 +47,7 @@ for iSession = 1:length(folders)
         % IterativeArtifactRemoval (OnionPeeling): Load spikes
         for iChunk = 1:(384/chunkSize)
             channels = (iChunk-1)*chunkSize + 1 : iChunk*chunkSize;
-            tr.LoadSpikes(channels, Path='Spikes_AutoSorted');
+            tr.LoadSpikes(channels, Path='Spikes');
             channels = [tr.Spikes.Channel];
 
             if isempty(channels)
@@ -90,7 +57,7 @@ for iSession = 1:length(folders)
             % tr.PlotAllChannels(Channels=channels, plotMethod='mean')
 
             % IterativeArtifactRemoval (OnionPeeling): kmeans, pca
-            tr.IterativeArtifactRemoval(channels, MinSpikeRate=4, KIterative=4, KFinal=2, MaxIters=5, ...
+            tr.IterativeArtifactRemoval(channels, MinSpikeRate=0.5, KIterative=4, KFinal=2, MaxIters=5, ...
                 DimensionIterative=3, DimensionFinal=10, FeatureMethod='PCA', ClusterMethod='kmeans', ...
                 WaveformWindow=[-0.5, 0.5]);
 
@@ -102,6 +69,33 @@ for iSession = 1:length(folders)
         warning('Error in program %s.\nTraceback (most recent at top):\n%s\nError Message:\n%s', mfilename, getcallstack(ME), ME.message)
     end
 end
+
+
+
+%% Load sorted data on a different PC
+clear, clc
+tr = TetrodeRecording();
+tr.SelectFiles();
+tr.LoadNeuropixelIO();
+tr.ParseNeuropixelIO();
+
+channels = 1:128;
+tr.LoadSpikes(channels, Path='Spikes_AutoSortedIterative');
+tr.PlotAllChannels(Channels=channels, plotMethod='mean')
+%%
+tr.SaveSpikes(Channels=channels, Path='Spikes_Sorted')
+channels = 129:256;
+tr.Spikes = [];
+tr.LoadSpikes(channels, Path='Spikes_AutoSortedIterative');
+tr.PlotAllChannels(Channels=channels, plotMethod='mean')
+%%
+tr.SaveSpikes(Channels=channels, Path='Spikes_Sorted')
+channels = 257:384;
+tr.Spikes = [];
+tr.LoadSpikes(channels, Path='Spikes_AutoSortedIterative');
+tr.PlotAllChannels(Channels=channels, plotMethod='mean')
+%%
+tr.SaveSpikes(Channels=channels, Path='Spikes_Sorted')
 
 %% Convert to EphysUnits
 folders = { ...
