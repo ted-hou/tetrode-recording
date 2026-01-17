78297f25fe4e47eac676f059dc7ef43dd57c6c85
ted-hou <ted_hou@126.com>
2026-01-16 19:01:06 -0500

    h

--- TetrodeRecording.m
+++ TetrodeRecording.m
@@ -322,7 +322,7 @@ classdef TetrodeRecording < handle
                             obj.SpikeDetect(1:size(obj.Amplifier.Data, 1), NumSigmas=numSigmas, NumSigmasReturn=numSigmasReturn, NumSigmasReject=numSigmasReject, ...
                                 WaveformWindow=waveformWindow, Direction=direction, Append=false, MaxMicroVolts=750, MinThresholdMicroVolts=45, MaxThresholdMicroVolts=75, UseClampedThresholdInsteadOfSigma=true);
                         end
-                        obj.ReadNIDQ(Duration=duration, ReadMode='single');
+                        obj.ReadNIDQ(TimeWindow=duration, ReadMode='simple');
                     end
                     
 			end
--- example/script_neuropixel_optrode_SC_ReverseInjection.m
+++ example/script_neuropixel_optrode_SC_ReverseInjection.m
@@ -10,7 +10,7 @@ for iSession = 1:length(folders)
     try
         tr = TetrodeRecording;
         tr.SelectFiles(NeuropixelPath=folders{iSession});
-        tr.ReadFiles(Duration=[0, 5], Channels=1:384, NumSigmas=4, NumSigmasReturn=1.5, NumSigmasReject=20, WaveformWindow=[-0.5, 1])
+        tr.ReadFiles(Duration=240, Channels=1:384, NumSigmas=4, NumSigmasReturn=1.5, NumSigmasReject=20, WaveformWindow=[-0.5, 1])
         tr.SaveNeuropixelIO()
         
         % Read detected spikes and NIDQ digital/analog channels
@@ -22,7 +22,7 @@ for iSession = 1:length(folders)
         tr.IterativeArtifactRemoval(1, MinSpikeRate=0.5, KIterative=4, KFinal=2, MaxIters=5, ...
             DimensionIterative=3, DimensionFinal=10, FeatureMethod='PCA', ClusterMethod='kmeans', ...
             WaveformWindow=[-0.5, 0.5]);
-        tr.SaveSpikes(Path='Spikes_AutoSortedIterative');
+        tr.SaveSpikes(Path='Spikes_Dummy');
     catch ME
         warning('Could not process folder: %s', folders{iSession})
         warning('Error in program %s.\nTraceback (most recent at top):\n%s\nError Message:\n%s', mfilename, getcallstack(ME), ME.message)
@@ -30,14 +30,14 @@ for iSession = 1:length(folders)
 end
 
 
-%% Convert to EphysUnits
+% Convert to EphysUnits
 folders = { ...
-    'C:\SERVER\daisy26\daisy26_20250521', ...
-    'C:\SERVER\desmond38\desmond38_20250520', ...
-    'C:\SERVER\desmond39\desmond39_20250522', ...
+    'C:\SERVER\daisy27\daisy27_20250707', ...
+    'C:\SERVER\daisy27\daisy27_20250715', ... % 16mW mirror 0, 100ms moved arm
+    'C:\SERVER\daisy28\daisy28_20250714', ... % 16mW mirror 0, 100ms moved arm
     };
 
-chunkSize = 32; % NumChannelsPerChunk
+chunkSize = 1; % NumChannelsPerChunk
 for iSession = 1:length(folders)
     try
         tr = TetrodeRecording();
@@ -48,7 +48,7 @@ for iSession = 1:length(folders)
         % IterativeArtifactRemoval (OnionPeeling): Load spikes
         for iChunk = 1:(384/chunkSize)
             channels = (iChunk-1)*chunkSize + 1 : iChunk*chunkSize;
-            tr.LoadSpikes(channels, Path='Spikes_Sorted');
+            tr.LoadSpikes(channels, Path='Spikes_Dummy');
 
             if isempty(tr.Spikes) || isempty([tr.Spikes.Channel])
                 tr.Spikes = [];
@@ -59,7 +59,7 @@ for iSession = 1:length(folders)
 
             ar = AcuteRecording(tr, 'N/A');
             ar.binMoveResponse(tr, 'none', Window=[-1, 0], Store=true);
-            eu = EphysUnit(ar, readWaveforms=false, cullITI=false, savepath='C:\SERVER\Units\TwoColor_SC\Batch2', tr=tr);
+            eu = EphysUnit(ar, readWaveforms=false, cullITI=false, savepath='C:\SERVER\Units\TwoColor_SC\ReverseInjection_Dummy', tr=tr);
 
             tr.Spikes = [];
             clear eu
