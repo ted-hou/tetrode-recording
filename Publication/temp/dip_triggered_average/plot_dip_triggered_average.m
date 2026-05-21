ed1ad39b7ed44d0a659de18049d3c22d88873503
ted-hou <tedhou1993@gmail.com>
2026-05-20 20:51:58 -0400

    Update plot_dip_triggered_average.m

--- Publication/temp/dip_triggered_average/plot_dip_triggered_average.m
+++ Publication/temp/dip_triggered_average/plot_dip_triggered_average.m
@@ -1,7 +1,12 @@
-clearvars -except xta p kinematics
+%% Set root
+ROOTPATH = "E:\DATA";
+% ROOTPATH = 'C:\SERVER';
+
+%% Clear temp vars
+clearvars -except xta p kinematics ROOTPATH
 
 %% Load data
-load('C:\SERVER\LickVsReach_DTA_RTA_boot\NewData\LickVsReach_DLC_dta_rta_25_25_200to800ms_units1to1443_100boots.mat');
+load(fullfile(ROOTPATH, "LickVsReach_DTA_RTA_boot\NewData\LickVsReach_DLC_dta_rta_25_25_200to800ms_units1to1443_100boots.mat"));
 
 %% Make a metaDTA/metaRTA
 for fn = ["spikerate", "HandR", "HandL", "FootR", "FootL", "Spine", "Jaw", "Tongue"]
@@ -19,13 +24,37 @@ xta.dip(length(eu) + 1).t0 = [xta.dip(selUnits).t0];
 xta.rise(length(eu) + 1).t0 = [xta.rise(selUnits).t0];
 
 %% Combine movement indices (mi) across dips from all units, then cluster them
+features = ["spikerate", "HandR", "HandL", "FootR", "FootL", "Spine", "Jaw", "Tongue"];
+featureUnits = ["spike rate (a.u.)", "AP pos (a.u.)", "AP pos (a.u.)", "AP pos (a.u.)", "AP pos (a.u.)", "DV pos (a.u.)", "DV pos (a.u.)", "prob"];
+featureSign = [1, 1, 1, 1, 1, -1, -1, 1];
+featureAxisDir = ["normal", "normal", "normal", "normal", "normal", "normal", "reverse", "normal"];
+ylims = {5, 3, 3, 3, 3, 3, 3, 1};
+ylims = cellfun(@(y) y*[-2/3, 1], ylims, UniformOutput=false);
+ylims{featureAxisDir=="reverse"} = [-1, 2/3]*3;
 p.mi.features = ["HandR", "HandL", "FootR", "FootL", "Spine", "Jaw", "Tongue"];
 p.mi.windowPre = [-1, -0.3];
 p.mi.windowPost = [0, 0.6];
+p.mi.nClusters = 8;
 p.mi.clusterMethod = "kmeans"; % "gaussian", "kmeans"
-p.mi.clusterDimensions = 4;
-p.mi.displayMethod = "umap"; % "pca", "tsne", "umap"
-p.mi.nClusters = 4;
+p.mi.clusterDimensions = 7;
+p.mi.dimensionReductionMethod = "umap"; % "pca", "tsne", "umap", "manual", "manual+umap"... manual: avg(4 limbs) vs. avg(tongue/jaw) vs. spine
+p.mi.displayDimensions = 2;
+switch p.mi.dimensionReductionMethod
+    case "pca"
+        p.mi.displayMethodName = "Movement Profile (PCA)";
+    case "tsne"
+        p.mi.displayMethodName = "Movement Profile (t-SNE)";
+    case "umap"
+        p.mi.displayMethodName = "Movement Profile (UMAP)";
+    case "manual"
+        p.mi.displayMethodName = "Movement Profile (Manual)";
+        assert(p.mi.clusterDimensions == 3)
+    case "manual+umap"
+        p.mi.displayMethodName = "Movement Profile (Manual + UMAP)";
+        assert(p.mi.clusterDimensions == 3)
+    otherwise
+        error("unknown method %s", p.mi.dimensionReductionMethod)
+end
 tTic = tic();
 clear mi
 for dir = ["dip", "rise"]
@@ -55,38 +84,60 @@ for fn = p.mi.features
     iFeat = iFeat + 1;
     X(:, iFeat) = vertcat(vertcat(mi.dip.(fn)), vertcat(mi.rise.(fn)));
 end
+if ismember(p.mi.dimensionReductionMethod, ["manual", "manual+umap"])
+    Y = NaN(size(X, 1), 3);
+    Y(:, 1) = mean(X(:, ismember(p.mi.features, ["HandL", "HandR", "FootL", "FootR"])), 2, 'omitnan');
+    Y(:, 2) = mean(X(:, ismember(p.mi.features, ["Spine"])), 2, 'omitnan');
+    Y(:, 3) = mean(X(:, ismember(p.mi.features, ["Tongue", "Jaw"])), 2, 'omitnan');
+    X = Y;
+    clear Y
+end
 X(isnan(X)) = 0;
 fprintf("(%.1fs)\n", toc(tTic));
-fprintf("PCA...")
-[~, pcaScoreMerge, ~, ~] = pca(X);
-fprintf("(%.1fs)\n", toc(tTic));
-switch p.mi.displayMethod
+fprintf("%s...", p.mi.displayMethodName)
+switch p.mi.dimensionReductionMethod
     case "pca"
-        fprintf("PCA...")
-        dispScoreMerge = pcaScoreMerge;
+        [~, pcaScoreMerge, ~, ~] = pca(X);
+        dispScoreMerge = pcaScoreMerge(:, 1:p.mi.displayDimensions);
     case "tsne"
-        fprintf("t-SNE...")
-        dispScoreMerge = tsne(X);
+        [~, pcaScoreMerge, ~, ~] = pca(X);
+        dispScoreMerge = tsne(X, NumDimensions=p.mi.displayDimensions);
     case "umap"
-        fprintf("UMAP...")
-        dispScoreMerge = umap(X);
+        [~, pcaScoreMerge, ~, ~] = pca(X);
+        dispScoreMerge = umap(X, NumDimensions=p.mi.displayDimensions);
+    case "manual"
+        pcaScoreMerge = X;
+        dispScoreMerge = X;
+    case "manual+umap"
+        pcaScoreMerge = X;
+        dispScoreMerge = umap(X, NumDimensions=p.mi.displayDimensions);
+    otherwise
+        error("unknown method %s", p.mi.dimensionReductionMethod)
 end
 fprintf("(%.1fs)\n", toc(tTic));
 
 fprintf("Clustering ")
 switch p.mi.clusterMethod
     case "kmeans"
-        % eva = evalclusters(pcaScore(:, 1:3), 'kmeans', 'CalinskiHarabasz', KList=2:10);
-        % nClusters = eva.OptimalK;
-        % clear eva
         fprintf("(kmeans)...")
+        % Fix the seed so we don't have to manually assign semantic cluster labels repeatedly
+        rng(1); p.mi.semanticClusterLabels = ["no move", "lick start", "lick stop", "left hand retract", "left hand reach", "right hand retract", "right hand reach", "left foot retract"];
         idxMerge = kmeans(pcaScoreMerge(:, 1:p.mi.clusterDimensions), p.mi.nClusters);
     case "gaussian"
         fprintf("(Gaussian mixture)...")
+        rng(1);
         gm = fitgmdist(pcaScoreMerge(:, 1:p.mi.clusterDimensions), p.mi.nClusters);
         idxMerge = cluster(gm, pcaScoreMerge(:, 1:p.mi.clusterDimensions));
         clear gm
+    otherwise
+        error("unknown method %s", p.mi.clusterMethod)
 end
+% Cluster indices should reflect cluster size
+n = arrayfun(@(k) nnz(idxMerge==k), 1:max(idxMerge));
+[~, clusterOrder] = sort(n, 'descend');
+idxMerge = changem(idxMerge, 1:max(idxMerge), clusterOrder);
+clear n clusterOrder
+
 fprintf("(%.1fs)\n", toc(tTic));
 
 % Reassign cluster indices to units
@@ -103,43 +154,158 @@ i0.rise = 0;
 for iUnit = 1:length(xta.dip)
     for dir = ["dip", "rise"]
         n = nTrials.(dir)(iUnit);
-        mi.(dir)(iUnit).idx = idx.dip(i0.(dir)+1 : i0.(dir)+n);
-        mi.(dir)(iUnit).pcaScore = pcaScore.dip(i0.(dir)+1 : i0.(dir)+n, :);
-        mi.(dir)(iUnit).dispScore = dispScore.dip(i0.(dir)+1 : i0.(dir)+n, :);
+        mi.(dir)(iUnit).idx = idx.(dir)(i0.(dir)+1 : i0.(dir)+n);
+        mi.(dir)(iUnit).pcaScore = pcaScore.(dir)(i0.(dir)+1 : i0.(dir)+n, :);
+        mi.(dir)(iUnit).dispScore = dispScore.(dir)(i0.(dir)+1 : i0.(dir)+n, :);
         i0.(dir) = i0.(dir) + n;
     end
 end
 assert(i0.dip == sum(nTrials.dip))
 assert(i0.rise == sum(nTrials.rise))
 
-%
-fprintf("Plotting...")
-ax = axes(figure());
+% Average movement profiles by cluster
+clear mp0 mp
+mp0(length(xta.dip), 1) = struct(spikerate=[], HandR=[], HandL=[], FootR=[], FootL=[], Spine=[], Jaw=[], Tongue=[]);
+mp = struct(dip=mp0, rise=mp0);
+for iUnit = 1:length(xta.dip)
+    for dir = ["dip", "rise"]
+        for k = 1:p.mi.nClusters
+            sel = mi.(dir)(iUnit).idx==k;
+            for fn = ["spikerate", "HandR", "HandL", "FootR", "FootL", "Spine", "Jaw", "Tongue"]
+                mp.(dir)(iUnit, k).(fn) = xta.(dir)(iUnit).(fn).X(sel, :);
+            end
+        end
+    end
+end
+clear mp0 iUnit dir k sel fn
+
+mpMean = struct(dip=[], rise=[]);
+for dir = ["dip", "rise"]
+    for fn = ["spikerate", "HandR", "HandL", "FootR", "FootL", "Spine", "Jaw", "Tongue"]
+        XCell = arrayfun(@(mp) mp.(fn), mp.(dir), UniformOutput=false);
+        t = xta.(dir)(1).(fn).t;
+        X = NaN(p.mi.nClusters, length(t));
+        for k = 1:p.mi.nClusters
+            X(k, :) = mean(cat(1, XCell{:, k}), 1, 'omitnan');
+        end
+        mpMean.(dir).(fn).X = X;
+        mpMean.(dir).(fn).t = t;
+        mpMean.(dir).(fn).N = sum(cellfun(@(x) size(x, 1), XCell), 1);
+    end
+end
+clear dir fn XCell X k t
+
+
+% Scatter plot of all movement profiles
+close all
+layout.h = [4, 1, 1];
+fig = figure(Units='normalized', Position=[0.05, 0.05, 0.9, 0.9]);
+tlp = tiledlayout(fig, sum(layout.h), 1);
+ax = nexttile(tlp, 1, [layout.h(1), 1]);
 hold(ax, 'on')
-for k = 1:p.mi.nClusters
-    sel = idxMerge==k;
-    scatter(ax, dispScoreMerge(sel, 1), dispScoreMerge(sel, 2), 4, getColor(k, p.mi.nClusters, 0.7))
+h = gobjects(2, p.mi.nClusters);
+iDir = 0;
+for dir = ["dip", "rise"]
+    iDir = iDir + 1;
+    for k = 1:p.mi.nClusters
+        sel = idx.(dir)==k;
+        switch dir
+            case "dip"
+                faceColor = "none";
+                style = 'o';
+            case "rise"
+                faceColor = [getColor(k, p.mi.nClusters, 0.7)];
+                style = 'o';
+        end
+
+        if p.mi.displayDimensions==2
+            h(iDir, k) = scatter(ax, dispScore.(dir)(sel, 1), dispScore.(dir)(sel, 2), 3, Marker=style, MarkerEdgeColor=[getColor(k, p.mi.nClusters, 0.7)], MarkerFaceColor=faceColor, DisplayName=sprintf('Clu%i (n=%i %ss)', k, nnz(sel), dir));
+            % scatter(ax, mean(dispScore.(dir)(sel, 1), 1, 'omitnan'), mean(dispScore.(dir)(sel, 2), 1, 'omitnan'), 300, Marker=style, MarkerEdgeColor=[getColor(k, p.mi.nClusters, 0.7)], MarkerFaceColor=faceColor, LineWidth=3);
+        else
+            h(iDir, k) = scatter3(ax, dispScore.(dir)(sel, 1), dispScore.(dir)(sel, 2), dispScore.(dir)(sel, 3), 3, Marker=style, MarkerEdgeColor=[getColor(k, p.mi.nClusters, 0.7)], MarkerFaceColor=faceColor, DisplayName=sprintf('Clu%i (n=%i %ss)', k, nnz(sel), dir));
+        end
+        clear faceColor style
+    end
+end
+% axis(ax, 'equal')
+xl = quantile(dispScoreMerge(:, 1), [0, 1]);
+yl = quantile(dispScoreMerge(:, 2), [0, 1]);
+xlim(ax, xl)
+ylim(ax, yl)
+plot3(ax, xl, [0, 0], [0, 0], 'k-')
+plot3(ax, [0, 0], yl, [0, 0], 'k-')
+if p.mi.displayDimensions == 3
+    zl = quantile(dispScoreMerge(:, 3), [0.01, 0.99]);
+    plot3(ax, [0, 0], [0, 0], zl, 'k-')
+    zlim(ax, zl)
+end
+switch p.mi.dimensionReductionMethod
+    case "manual"
+        xlabel(ax, 'Hands/Feet')
+        ylabel(ax, 'Tongue/Jaw')
+        if p.mi.displayDimensions == 3
+            zlabel(ax, 'Spine')
+        end
+    otherwise
+        xlabel(ax, 'PC1')
+        ylabel(ax, 'PC2')
+        if p.mi.displayDimensions == 3
+            zlabel(ax, 'PC3')
+        end
 end
 hold(ax, 'off')
-fprintf("(%.1fs)\n", toc(tTic));
+legend(h, Location='northeast')
+title(ax, p.mi.displayMethodName)
+
+% Plot grand average movement trajectories by cluster
+lineStyles = ["-", "-", "-", "-", "--", "-", "--", "-"];
+tl = gobjects(2, 1);
+for iDir = 1:2
+    tl(iDir) = tiledlayout(tlp, 1, length(features));
+    tl(iDir).Layout.Tile = sum(layout.h(1:iDir)) + 1;
+    tl(iDir).Layout.TileSpan = [layout.h(iDir+1), 1];
+end
+iAx = 0;
+h = gobjects(2, length(features), p.mi.nClusters);
+for iFeat = 1:length(features)
+    fn = features(iFeat);
+    iAx = iAx + 1;
+    iDir = 0;
+    for dir = ["dip", "rise"]
+        iDir = iDir + 1;
+        ax = nexttile(tl(iDir));
+        hold(ax, 'on')
+        for k = 1:p.mi.nClusters
+            h(iDir, iAx, k) = plot(ax, mpMean.(dir).(fn).t, featureSign(iFeat)*mpMean.(dir).(fn).X(k, :), Color=[getColor(k, p.mi.nClusters, 0.7)], LineStyle=lineStyles(k), LineWidth=1.5, DisplayName=sprintf('Clu%i (n=%i)', k, mpMean.(dir).(fn).N(k)));
+        end
+        hold(ax, 'off')
+        title(ax, fn);
+        xlabel(ax, 'time (ms)')
+        ylabel(ax, featureUnits(iAx))
+        yline(ax, 0, Color=[0.15, 0.15, 0.15, 0.5], LineStyle=':')
+        xline(ax, 0, Color=[0.15, 0.15, 0.15, 0.5], LineStyle=':')
+        ylim(ax, ylims{iAx})
+        ax.YAxis.Direction = featureAxisDir(iFeat);
+    end
+end
+legend(h(1, length(features), :), Location='eastoutside')
+legend(h(2, length(features), :), Location='eastoutside')
+
+fontsize(fig, 9, 'points')
 
 clear tTic iUnit dir fn X t XPre XPost
-clear iFeat fn pcaScoreMerge pcaExplained dispScoreMerge nClusters idxMerge ax k sel
-clear idx pcaScore dispScore i0 iUnit dir n
+clear iFeat fn pcaScoreMerge pcaExplained nClusters ax k sel
+clear i0 iUnit dir n h
+clear fig ax tl tlp layout iAx fn k faceColor dir iDir xl yl zl
 %% Plot dip-triggered average kinematics (STD Version
 close all
-exportPath = fullfile("C:\SERVER\LickVsReach_DTA_RTA_boot\NewData\Figures", sprintf("LickVsReach_DLC_dta_rta_%i_%i_%ito%ims_std", 100*p.xta.dip.thresholdQuantile, 100*p.xta.dip.thresholdSubQuantile, 100*p.xta.dip.samples(1), 100*p.xta.rise.samples(2)));
+exportPath = fullfile(ROOTPATH, "LickVsReach_DTA_RTA_boot\NewData\Figures", sprintf("LickVsReach_DLC_dta_rta_%i_%i_%ito%ims_std", 100*p.xta.dip.thresholdQuantile, 100*p.xta.dip.thresholdSubQuantile, 100*p.xta.dip.samples(1), 100*p.xta.rise.samples(2)));
 if ~exist(exportPath, 'dir')
     mkdir(exportPath)
 end
-features = ["spikerate", "HandR", "HandL", "FootR", "FootL", "Spine", "Jaw", "Tongue"];
-featureUnits = ["spike rate (a.u.)", "AP pos (a.u.)", "AP pos (a.u.)", "AP pos (a.u.)", "AP pos (a.u.)", "DV pos (a.u.)", "DV pos (a.u.)", "prob"];
 statFeatures = ["HandR", "HandL", "FootR", "FootL", "Spine", "Jaw", "Tongue"];
-nAx = length(features) + 2;
+nAx = length(features);
 dirs = ["dip", "rise"];
-dimensionReduction = "tsne"; % pca, tsne, umap
-% yl = {[-2.5, 5], [-0.1, 5.1], [-0.1, 5.1], [-0.1, 5.1], [-0.1, 5.1], [-0.1, 5.1], [-0.1, 5.1], [-0.1, 1.1]};
-yl = {[-2.5, 5], [-4, 4], [-4, 4], [-4, 4], [-4, 4], [-4, 4], [-4, 4], [0, 1]};
 fig = figure(Units='inches', InnerPosition=[2, 2, 1.5*(2+length(features)), 4.5]);
 tlp = tiledlayout(fig, 2, 1, TileSpacing='compact', Padding='compact');
 tl = gobjects(2, 1);
@@ -174,33 +340,33 @@ for iUnit = 1:length(xta.dip)
         % Movement diversity scatter plot
         [lia, statFeatureOrder] = ismember(statFeatures, p.std.features);
         assert(all(lia), 'Some members of statFeatureOrder are not found.')
-        
-        if p.nBoot > 0 && xta.(dir)(iUnit).iExp > 0
-            iAx = nAx - 1;
-            hold(ax(iDir, iAx), 'on')
-            idx = mi.(dir)(iUnit).idx;
-            score = mi.(dir)(iUnit).dispScore;
-            for k = 1:p.mi.nClusters
-                sel = idx==k;
-                if nnz(sel) == 0
-                    continue
-                end
-                scatter(ax(iDir, iAx), score(sel, 1), score(sel, 2), 10, getColor(k, p.mi.nClusters, 0.7))
-            end
-            xlabel(ax(iDir, iAx), sprintf("PC%i", 1))
-            ylabel(ax(iDir, iAx), sprintf("PC%i", 2))
-            switch p.mi.displayMethod   
-                case "pca"
-                    title(ax(iDir, iAx), "PCA")
-                case "tsne"
-                    title(ax(iDir, iAx), "t-SNE")
-                case "umap"
-                    title(ax(iDir, iAx), "UMAP")
-            end
-            xticks(ax(iDir, iAx), [])
-            yticks(ax(iDir, iAx), [])
-            hold(ax(iDir, iAx), 'off')
-        end
+
+        idx = mi.(dir)(iUnit).idx;
+        % if p.nBoot > 0 && xta.(dir)(iUnit).iExp > 0
+            % iAx = nAx - 1;
+            % hold(ax(iDir, iAx), 'on')
+            % score = mi.(dir)(iUnit).dispScore;
+            % for k = 1:p.mi.nClusters
+            %     sel = idx==k;
+            %     if nnz(sel) == 0
+            %         continue
+            %     end
+            %     scatter(ax(iDir, iAx), score(sel, 1), score(sel, 2), 10, getColor(k, p.mi.nClusters, 0.7))
+            % end
+            % xlabel(ax(iDir, iAx), sprintf("PC%i", 1))
+            % ylabel(ax(iDir, iAx), sprintf("PC%i", 2))
+            % switch p.mi.dimensionReductionMethod   
+            %     case "pca"
+            %         title(ax(iDir, iAx), "PCA")
+            %     case "tsne"
+            %         title(ax(iDir, iAx), "t-SNE")
+            %     case "umap"
+            %         title(ax(iDir, iAx), "UMAP")
+            % end
+            % xticks(ax(iDir, iAx), [])
+            % yticks(ax(iDir, iAx), [])
+            % hold(ax(iDir, iAx), 'off')
+        % end
 
         % % Movement diversity matrix
         % if p.nBoot > 0 && xta.(dir)(iUnit).iExp > 0
@@ -254,26 +420,28 @@ for iUnit = 1:length(xta.dip)
             ax(iDir, iAx).Visible = true;
 
             hold(ax(iDir, iAx), 'on')
-            hold(ax(2+1-iDir, iAx), 'on')
+            % hold(ax(2+1-iDir, iAx), 'on')
             t = 1e3*xta.(dir)(iUnit).(fn).t;
-            X = mean(xta.(dir)(iUnit).(fn).X, 1, 'omitnan');
-            plot(ax(iDir, iAx), t, X, Color=[0.15, 0.15, 0.15, 1], LineWidth=1.5, LineStyle=':');
-            plot(ax(2+1-iDir, iAx), t, X, Color=[0.15, 0.15, 0.15, 0.1], LineWidth=1.5, LineStyle=':');
+            plot(ax(iDir, iAx), t, featureSign(iAx)*mean(xta.(dir)(iUnit).(fn).X, 1, 'omitnan'), Color=[0.15, 0.15, 0.15, 1], LineWidth=1.5, LineStyle=':');
+            % plot(ax(2+1-iDir, iAx), t, X, Color=[0.15, 0.15, 0.15, 0.1], LineWidth=1.5, LineStyle=':');
+            h = gobjects(p.mi.nClusters, 1);
+            isClusterEmpty = false(1, p.mi.nClusters);
             for k = 1:p.mi.nClusters
                 sel = idx==k;
                 if nnz(sel) == 0
+                    isClusterEmpty(k) = true;
                     continue
                 end
                 c = getColor(k, p.mi.nClusters, 0.7);
                 X = xta.(dir)(iUnit).(fn).X(sel, :);
-                mu = mean(X, 1, 'omitnan');
-                err = std(X, 0, 1, 'omitnan')./sqrt(size(xta.(dir)(iUnit).(fn).X, 1));
+                mu = featureSign(iAx)*mean(X, 1, 'omitnan');
+                % err = std(X, 0, 1, 'omitnan')./sqrt(size(xta.(dir)(iUnit).(fn).X, 1));
     
-                plot(ax(iDir, iAx), t, mu, Color=c, LineWidth=1.5);
+                h(k) = plot(ax(iDir, iAx), t, mu, Color=c, LineStyle=lineStyles(k), LineWidth=1.5, DisplayName=sprintf("%s (n=%i)", p.mi.semanticClusterLabels(k), nnz(sel)));
             end
             if p.nBoot > 0 && isfield(xta.(dir)(iUnit).(fn), 'XBoot')
                 prc = quantile(xta.(dir)(iUnit).(fn).XBoot, [p.bootAlpha/2, 1-p.bootAlpha/2], 1);
-                patch(ax(iDir, iAx), [t, flip(t)], [prc(1, :), flip(prc(2, :))], [0.15, 0.15, 0.15], FaceAlpha=0.05, EdgeColor=[0.15, 0.15, 0.15], EdgeAlpha=0.5);
+                patch(ax(iDir, iAx), [t, flip(t)], featureSign(iAx)*[prc(1, :), flip(prc(2, :))], [0.15, 0.15, 0.15], FaceAlpha=0.05, EdgeColor=[0.15, 0.15, 0.15], EdgeAlpha=0.5);
             end
             % xline(ax(iRow, iAx), 1e3*p.xta.meanWindow, 'k--', Alpha=0.1)
             xline(ax(iDir, iAx), 1e3*p.std.window, 'k--', Alpha=0.1)
@@ -287,10 +455,13 @@ for iUnit = 1:length(xta.dip)
 
             fnDisp = sprintf("%s %s", fn, repmat('*', [1, nStarsSTD]));
             title(ax(iDir, iAx), fnDisp, Interpreter='none')
-            ylim(ax(iDir, iAx), yl{iAx})
+            ylim(ax(iDir, iAx), 3*ylims{iAx})
             hold(ax(iDir, iAx), 'off')
-            hold(ax(2+1-iDir, iAx), 'off')
+            ax(iDir, iAx).YAxis.Direction = featureAxisDir(iAx);
+            % hold(ax(2+1-iDir, iAx), 'off')
         end
+        lgd = legend(ax(iDir, iAx), h(~isClusterEmpty), AutoUpdate=false);
+        lgd.Layout.Tile = 'east';
 
         % Correlegram
         % 
@@ -343,7 +514,7 @@ for iUnit = 1:length(xta.dip)
     print(fig, fullfile(exportPath, sprintf("dta_rta_unit_%03i", iUnit)), '-dpng', '-r0')
 end
 
-clear exportPath features featureUnits statFeatures nAx dirs dimensionReduction yl fig tlp tl ax iDir iAx iUnit dir
+clear exportPath statFeatures nAx dirs dimensionReduction fig tlp tl ax iDir iAx iUnit dir
 clear lia statFeatureOrder idx nClusters mr i fn t selT xx pcScore explained score eva k sel
 clear mdm i fn t selT stdObs hash I idxSorted sepHash prcSTD nStarsSTD t X k c mu err prc
-clear iDir dir
+clear iDir dir fnDisp
